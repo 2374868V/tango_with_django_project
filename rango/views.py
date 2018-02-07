e73f5dbea5457228f4ce41b05d955f78396167f1
Guus van der Ham <2374868v@student.gla.ac.uk>
2018-02-07 14:42:48 +0000

    first commit chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from rango.models import Category
+from rango.models import Page
 
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage':"cruncy, creamy, cookie, candy, cupcake"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     context_dict = {'tutorialmessage':"this tutorial has been put together by Guus van der Ham"}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
